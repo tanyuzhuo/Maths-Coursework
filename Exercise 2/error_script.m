1ab0d54b38b10b656fbb3cc9de425e8bc4f4d38e
EdwardStables <edward.stables1198@gmail.com>
2018-03-20 12:30:53 +0000

    Updated error_script and RCL_script

--- Exercise 2/error_script.m
+++ Exercise 2/error_script.m
@@ -11,23 +11,17 @@ f = 1/T; %Cosine frequency
 %Equations
 Vin =@(t) 5*cos(2*pi*f*t);
 eqn=@(t,Vout) ((Vin(t)-Vout/c)/r);
+%To obtain the results for the 3 different RK2 methods, change the
+%commented lines in the RK2 function
 [t,y] = RK2(eqn, qi, [ti tf], h);
-Vout = y/c; %This section deals with charge, therefore the output needs to be converted to voltage
+Vout = y/c; %This section deals with charge, therefore the output needs to
+%be converted to voltage
+
 exact= ((5*C*cos(w*t)+5*w*R*C^2*sin(w*t)+... %Exact derivation is included in the report
     (qi*(1+4*pi^2)-5*C)*exp(-t/(R*C)))/((1+(w*R*C)^2)))/(100*10^-9);
 error=exact-Vout; %Error calculated as the difference between 
 
 %Output plotting
-figure
-plot(t, Vout, 'b');
-title('RK2')
-xlabel 't(s)'
-ylabel 'Vout(V)'
-figure; 
-plot(t, exact,'r');
-title('Exact')
-xlabel 't/s'
-ylabel 'Vout/V'
 figure;
 plot(t, error,'g');
 title('Error')
--- Exercise 3/RCL_script.m
+++ Exercise 3/RCL_script.m
@@ -7,13 +7,113 @@ C=3.5e-6;
 R=250;
 init1 = 0; %the initial inductor current
 init2 = 500e-9; %the initial capacitor charge
- 
 
+
+%step
+name = '5V step input';
 Vin = @(t)  5*(t>=0);
 eqn1 = @(x, y, z) z; %dy/dx
 eqn2 = @(x, y, z) 1/L*(Vin(x) - 1/C*y - R*z); %dz/dx 
-[x,y,z] = RK4(eqn1, eqn2, init1, init2, [0 0.03], 0.0000001);
+[x,y,z] = RK4(eqn1, eqn2, init1, init2, [0 0.035], 0.000001);
+Vout = R*z;
+figure('Name',name,'NumberTitle','off');
+plot(x, Vout);
+title(name)
+xlabel('Time (s)')
+ylabel('Voltage (V)')
+% hold on;
+
+%impulse
+name = 'Vin = 5V Impulse';
+tau = 3e-6;
+Vin = @(t) 5*exp(-t^2/tau);
+eqn1 = @(x, y, z) z; %dy/dx
+eqn2 = @(x, y, z) 1/L*(Vin(x) - 1/C*y - R*z); %dz/dx 
+[x,y,z] = RK4(eqn1, eqn2, init1, init2, [0 0.035], 0.000001);
+Vout = R*z;
+figure('Name',name,'NumberTitle','off');
+plot(x, Vout);
+title(name)
+xlabel('Time (s)')
+ylabel('Voltage (V)')
+
+%square 109
+name = 'Vin = Square Wave, A=5, f=109Hz';
+figure;
+subplot(2,2,1);
+Vin = @(t) 5*square(t*2*pi*109);
+eqn1 = @(x, y, z) z; %dy/dx
+eqn2 = @(x, y, z) 1/L*(Vin(x) - 1/C*y - R*z); %dz/dx 
+[x,y,z] = RK4(eqn1, eqn2, init1, init2, [0 0.035], 0.000001);
 Vout = R*z;
 plot(x, Vout);
-end
+title(name)
+xlabel('Time (s)')
+ylabel('Voltage (V)')
 
+%square 5
+name = 'Vin = Square Wave, A=5, f=5Hz';
+subplot(2,2,2);
+Vin = @(t) 5*square(t*2*pi*5);
+eqn1 = @(x, y, z) z; %dy/dx
+eqn2 = @(x, y, z) 1/L*(Vin(x) - 1/C*y - R*z); %dz/dx
+[x,y,z] = RK4(eqn1, eqn2, init1, init2, [0 0.23], 0.000001);
+Vout = R*z;
+plot(x, Vout);
+title(name)
+xlabel('Time (s)')
+ylabel('Voltage (V)')
+
+%square 500
+name = 'Vin = Square Wave, A=5, f=500Hz';
+subplot(2,2,3);
+Vin = @(t) 5*square(t*2*pi*500);
+eqn1 = @(x, y, z) z; %dy/dx
+eqn2 = @(x, y, z) 1/L*(Vin(x) - 1/C*y - R*z); %dz/dx
+[x,y,z] = RK4(eqn1, eqn2, init1, init2, [0 0.03], 0.000001);
+Vout = R*z;
+plot(x, Vout);
+title(name)
+xlabel('Time (s)')
+ylabel('Voltage (V)')
+
+%sine 109
+name = 'Vin = Sine Wave, A=5, f=109Hz';
+figure;
+subplot(2,2,1);
+Vin = @(t) 5*sin(t*2*pi*109);
+eqn1 = @(x, y, z) z; %dy/dx
+eqn2 = @(x, y, z) 1/L*(Vin(x) - 1/C*y - R*z); %dz/dx
+[x,y,z] = RK4(eqn1, eqn2, init1, init2, [0 0.035], 0.000001);
+Vout = R*z;
+plot(x, Vout);
+title(name)
+xlabel('Time (s)')
+ylabel('Voltage (V)')
+
+%sine 5
+name = 'Vin = Sine Wave, A=5, f=5Hz';
+subplot(2,2,2);
+Vin = @(t) 5*sin(t*2*pi*5);
+eqn1 = @(x, y, z) z; %dy/dx
+eqn2 = @(x, y, z) 1/L*(Vin(x) - 1/C*y - R*z); %dz/dx
+[x,y,z] = RK4(eqn1, eqn2, init1, init2, [0 0.25], 0.000001);
+Vout = R*z;
+plot(x, Vout);
+title(name)
+xlabel('Time (s)')
+ylabel('Voltage (V)')
+
+%sine 500
+name = 'Vin = Sine Wave, A=5, f=500Hz';
+subplot(2,2,3);
+Vin = @(t) 5*sin(t*2*pi*500);
+eqn1 = @(x, y, z) z; %dy/dx
+eqn2 = @(x, y, z) 1/L*(Vin(x) - 1/C*y - R*z); %dz/dx
+[x,y,z] = RK4(eqn1, eqn2, init1, init2, [0 0.02], 0.000001);
+Vout = R*z;
+plot(x, Vout);
+title(name)
+xlabel('Time (s)')
+ylabel('Voltage (V)')
+end
